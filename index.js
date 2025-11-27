b866495bc4c63b30a9932c3a54043c9dee85d116
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-11-27 08:02:49 +0530

    url-shortner

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app= express();
+
+const PORT =process.env.PORT ?? 8000
+
+app.get('/',(req, res) =>{
+    console.log(`Server is running.....`); 
+      
+})
+
+
+app.listen(PORT,() => {
+    console.log(`Server is running on PORT ${PORT}`);
+    
+})
