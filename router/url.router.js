de9b67967eb38cd26e98ed34ea1d3aea5ac03f19
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-11-29 10:37:00 +0530

    Fetching All Shortened URLs by the Logged-In Use

--- router/url.router.js
+++ router/url.router.js
@@ -1,55 +1,68 @@
-import express from 'express';
-import { shortenPostRequestBodySchema } from '../validation/request.validation.js';
-import {db} from '../db/index.js'
-import {urlsTable} from '../model/index.js';
-import { nanoid } from 'nanoid';
-import { ensureAuthenticated } from '../middleware/auth.middleware.js';
-import { eq } from 'drizzle-orm';
+import express from "express";
+import { shortenPostRequestBodySchema } from "../validation/request.validation.js";
+import { db } from "../db/index.js";
+import { urlsTable } from "../model/index.js";
+import { nanoid } from "nanoid";
+import { ensureAuthenticated } from "../middleware/auth.middleware.js";
+import { eq } from "drizzle-orm";
 
-const router =express.Router();
+const router = express.Router();
 
+router.post("/shorten", ensureAuthenticated, async function (req, res) {
+  const validationResult = await shortenPostRequestBodySchema.safeParseAsync(
+    req.body
+  );
 
-router.post('/shorten',ensureAuthenticated,async function (req,res) {
-    const validationResult =await shortenPostRequestBodySchema.safeParseAsync(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error });
+  }
 
-    if(validationResult.error){
-        return res.status(400).json({error:validationResult.error})
-    }
+  const { url, code } = validationResult.data;
 
-    const  {url,code} =validationResult.data;
+  const shortCode = code ?? nanoid(6);
 
-    const  shortCode =code ?? nanoid(6)
-
-    const [result]=await db.insert(urlsTable).values({
-        shortCode,
-        targetURL:url,
-        userId:req.user.id,
-        
-    }).returning({id:urlsTable.id,
-        shortCode:urlsTable.shortCode,
-        targetURL:urlsTable.targetURL
+  const [result] = await db
+    .insert(urlsTable)
+    .values({
+      shortCode,
+      targetURL: url,
+      userId: req.user.id,
+    })
+    .returning({
+      id: urlsTable.id,
+      shortCode: urlsTable.shortCode,
+      targetURL: urlsTable.targetURL,
     });
-    return res
-        .status(201)
-        .json({id:result.id,
-            shortCode:result.shortCode,
-            targetURL:result.targetURL
-        })
-    
-})
+  return res
+    .status(201)
+    .json({
+      id: result.id,
+      shortCode: result.shortCode,
+      targetURL: result.targetURL,
+    });
+});
 
+router.get("/code", ensureAuthenticated, async function (req, res) {
+  const code = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.userId, req.user.id));
 
-router.get('/:shortcode',async function (req,res) {
-    const code =req.params.shortcode;
-    const [result] =await db.select({targetURL:urlsTable.targetURL}).from(urlsTable).where(eq(urlsTable.shortCode,code))
+  return res.json({ code });
+});
 
-    if(!result ){
-        return res.status(404).json({error:'Invalid URL'})
-    }
+router.get("/:shortcode", async function (req, res) {
+  const code = req.params.shortcode;
+  const [result] = await db
+    .select({ targetURL: urlsTable.targetURL })
+    .from(urlsTable)
+    .where(eq(urlsTable.shortCode, code));
 
-    return res.status(404).json({error:"Invalid URL"})
-})
+  if (!result) {
+    return res.status(404).json({ error: "Invalid URL" });
+  }
 
-
+  return res.status(404).json({ error: "Invalid URL" });
+});
 
 export default router;
